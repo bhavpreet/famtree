3583e3400cbeceaa57130a893ffac99467fbeea0
Natasha Singh <31644074+nat027@users.noreply.github.com>
2020-12-06 23:10:09 +0530

    storing entry values in nodes/leaves
    New entry shows up a new node/ leaf with a highlight color & branch
    Mouse hover reveals Name;
    splendid :)

--- branch.js
+++ branch.js
@@ -3,42 +3,40 @@ function Branch (begin, end) {
 	this.end = end;
 	this.finished = false;
 
-	this.jitter = function () {
-		this.end.x += random(-0.125,0.125);
-		this.end.y += random(-0.125,0.125);
+	this.jitter = function (p) {
+		this.end.x += p.random(-0.125,0.125);
+		this.end.y += p.random(-0.125,0.125);
 	}
 
-	this.show = function () {
+	this.show = function (p) {
 		//making of the first branch (root)
-		stroke (0);
-		strokeWeight(2);
-		line (this.begin.x, this.begin.y, this.end.x, this.end.y);
+		p.stroke (0);
+		p.strokeWeight(2);
+		p.line (this.begin.x, this.begin.y, this.end.x, this.end.y);
 	}
 
-	this.branchN = function () {
+	this.branchR = function (p) {
 		//taking the vector angle, but first giving its direction
 		var dir = p5.Vector.sub(this.end, this.begin);
 		//p5 vector direction has an inbuilt angle
-		dir.rotate(PI/6);
+		dir.rotate(p.PI/4);
 		//making the length shorter than prev one
 		dir.mult(0.67);
 		//making a new end point for the new branch
 		var newEnd = p5.Vector.add(this.end, dir);
-		//new branch shares the previous branch's ending point 
+		//new branch shares the previous branch's ending point
 		//as its starting point plus we just made a new end for it
 		//in the previous line
 		var b = new Branch (this.end, newEnd);
 		return b;
 	};
 
-	this.branchB = function () {
-	
+	this.branchL = function (p) {
 		var dir = p5.Vector.sub(this.end, this.begin);
-		dir.rotate(-PI/4);
+		dir.rotate(-p.PI/6);
 		dir.mult(0.67);
 		var newEnd = p5.Vector.add(this.end, dir);
 		var b = new Branch (this.end, newEnd);
 		return b;
 	};
-
 }
--- sketch.js
+++ sketch.js
@@ -6,117 +6,170 @@
 // 2.publish on web, html (done: localhost)
 // 3.input field, show when hover
 // 4.define input fields, eg. color code as per age
-
-
-//level of interaction : keypress n and b
-
-//var angle = PI/4; //45 degree
-//var len = 50;
-//var root;
-
-//instead of making a root, making an array of tree and storing
-//the root and other branch values in the array
+var gp;
 var tree = [];
 var leaves = [];
-
 var count = 0;
+var table;
+var treeB = [];
+var levelB = 0;
+var treeN = [];
+var levelN = 0;
+
+
+let sketch = function(p) {
+    gp = p;
+    //level of interaction : keypress n and b
+
+    //var angle = PI/4; //45 degree
+    //var len = 50;
+    //var root;
+
+    //instead of making a root, making an array of tree and storing
+    //the root and other branch values in the array
+
+    p.preload = function() {
+        table = p.loadTable('https://docs.google.com/spreadsheets/d/1ugOJeRIHwUR36fp1-MawZqqw1_X29q4nrLiKM_gu9FI/gviz/tq\?tqx\=out:csv\&sheet\=Sheet1', 'csv', 'header');
+    }
+
+    p.setup = function() {
+        p.createCanvas(p.windowWidth, 400);
+        // p.createCanvas(400, 400);
+        p.textSize(23);
+
+
+        // vector here stores the start point and end point
+        // a vector has magnitude and direction
+        var a = p.createVector (p.width/2, p.height); //startpoint
+        var b = p.createVector (p.width/2, p.height-100); //endpoint
+        var root = new Branch (a, b); //starting first branch line |
+        tree[0] = root; //storing the root in the tree array
+        tree.push(tree[0].branchL(p))
+        treeN.push(tree[tree.length-1]);
+        tree.push(tree[0].branchR(p))
+        treeB.push(tree[tree.length-1]);
+
+        //var newBranch = root.branch(); //new branch came out of the root
+        //tree[1] = newBranch;
+        //console.log(tree);
+        drawTable(table);
+    }
+
+    p.draw = function() {
+
+        p.background(220);
+
+        // showing tree array
+        for ( var i = 0; i < tree.length; i++) {
+            //tree[1].show(); //replacing root.show, now array
+            //tree[2].show();
+            tree[i].show(p);
+            // tree[i].jitter(p);
+        }
+
+        //root.show();
+
+        for ( var i = 0; i < leaves.length; i++) {
+            if (leaves[i].entry.isNew == true) {
+                p.stroke(0);
+                p.fill(90, 0, 230);
+            } else {
+                p.noStroke();
+                p.fill(245, 156, 67);
+            }
+            //p.stroke(0);
+            p.ellipse (leaves[i].x, leaves[i].y, 8,8); //ellipse diam
+
+            //leaves[i].show();
+            let d = p.dist(p.mouseX, p.mouseY, leaves[i].x, leaves[i].y);
+            if (d < 5 || leaves[i].entry.isNew == true) {
+                p.fill(0);
+                p.textAlign(p.CENTER);
+                p.textSize(14);
+                //p.noStroke();
+                p.text(leaves[i].entry.name, leaves[i].x, leaves[i].y + 8 - 20);
+                p.fill(234,98,0);
+                p.stroke(0);
+                p.ellipse(leaves[i].x, leaves[i].y, 8,8);
+            }
+            
+        }
+    }
+};
+
+new p5(sketch, 'sketchCanvas');
+
+function drawEvenOdd(bnTree, level, entry) {
+    if ((bnTree.length - (2 * level + 1)) % 2 == 0) {
+        // console.log("even");
+        parent = (bnTree.length - 1) / 2;
+        // console.log("parent: ", parent);
+        // console.log("parent node: ", bnTree[parent]);
+        tree.push(bnTree[parent].branchL(gp));
+    } else {
+        // console.log("odd");
+        parent = (bnTree.length - 2) / 2;
+        // console.log("parent: ", parent);
+        // console.log("parent node: ", bnTree[parent]);
+        tree.push(bnTree[parent].branchR(gp));
+    }
+    bnTree.push(tree[tree.length-1]);
+    tree[tree.length-1].entry = entry;
+}
+
+function drawBranch(entry, isNew) {    // mouseClick
+    // console.log("entry = ", entry.relatedTo);
+    if (entry.relatedTo == "Natasha") {
+        var maxElems = Math.pow(2, levelN);
+        // console.log("maxElems = ", maxElems, "levelN = ", levelN)
+        if (treeN.length - (2 * levelN + 1) == maxElems) {
+            levelN += 1
+        }
+        drawEvenOdd(treeN, levelN, entry);
+    } else if (entry.relatedTo == "Bhavpreet") {
+        var maxElems = Math.pow(2, levelN);
+        // console.log("maxElems = ", maxElems, "levelN = ", levelN)
+        if (treeB.length - (2 * levelB + 1) == maxElems) {
+            levelB += 1
+        }
+        drawEvenOdd(treeB, levelB, entry);
+    }
+        var leaf = tree[tree.length-1].end.copy(); 
+        entry.isNew = isNew;
+        leaf.entry = entry;
+        leaves.push(leaf);
+
+    //color code name with alpha, rsvp
+    //age 
+    //add wedding info
+    //new entry- new color, details- name
+    
+}
+
+
+// Call Draw branch for each element in the table
+function drawTable(table) {
+    // console.log("table: ", table);
+    for (let r=0; r < table.getRowCount(); r++) {
+        entry = { "name" : table.getString(r, 0),
+                  "relation" : table.getString(r,1),
+                  "age" : table.getNum(r,2),
+                  "relatedTo" : table.getString(r,3),
+                  "rsvp" : table.getString(r,4)
+                };
+        drawBranch(entry, false);
+    }
+}
+
 
 // Save
 function saveToFamTree(name, relation) {
-  const Http = new XMLHttpRequest();
-  const url = 'https://script.google.com/macros/s/AKfycbzfGamhVtRvxDPyiqf9yofRX-GdJYGd6HzSx6sITtlgmQv0aJ0/exec?'
-  Http.open("GET", url + "col1=" + name + "&col2=" + relation);
-  Http.send();
+    const Http = new XMLHttpRequest();
+    const url = 'https://script.google.com/macros/s/AKfycbzfGamhVtRvxDPyiqf9yofRX-GdJYGd6HzSx6sITtlgmQv0aJ0/exec?'
+    Http.open("GET", url + "col1=" + name + "&col2=" + relation);
+    Http.send();
 
-  Http.onreadystatechange = (e) => {
-    console.log(Http.responseText)
-  }
-}
-
-let table;
-
-function preload() {
-  //my table is comma separated value "csv"
-  //and has a header specifying the columns labels
-  table = loadTable('https://docs.google.com/spreadsheets/d/1ugOJeRIHwUR36fp1-MawZqqw1_X29q4nrLiKM_gu9FI/gviz/tq\?tqx\=out:csv\&sheet\=Sheet1', 'csv', 'header');
-  //the file can be remote
-  //table = loadTable("http://p5js.org/reference/assets/mammals.csv",
-  //                  "csv", "header");
-}
-
-
-function setup() {
-  createCanvas(400, 400);
-  textSize(23);
-
-  for (let r=0; r < table.getRowCount(); r++) {
-    const name = table.getString(r, 0);
-    console.log(name);
-  }
-
-  // vector here stores the start point and end point
-  // a vector has magnitude and direction
-  var a = createVector (width/2, height); //startpoint
-  var b = createVector (width/2, height-100); //endpoint
-  var root = new Branch (a, b); //starting first branch line |
-
-  tree[0] = root; //storing the root in the tree array
-
-  //var newBranch = root.branch(); //new branch came out of the root
-  //tree[1] = newBranch;
-  //console.log(tree);
-}
-
-function mouseClicked () {
-  for (var i = tree.length-1; i >= 0; i--) {
-    if (!tree[i].finished) {
-      tree.push(tree[i].branchN()); //add an array to the tree by .push
-      tree.push(tree[i].branchB());
+    Http.onreadystatechange = (e) => {
+        console.log(Http.responseText)
     }
-    tree[i].finished = true;
-  }
-    count++;
-
-    if (count === 4) {
-      for ( var i = 0; i< tree.length; i++) {
-          if (!tree[i].finished){
-            var leaf = tree[i].end.copy(); //grab end point of branch for the leaf
-            leaves.push(leaf);
-          }
-      }
-    }
-  }
-  // tree[1] = tree[0].branchN(); //instead of root.branchA, since tree[0] global 
-  // tree[2] = tree[0].branchB();
-
-
-function draw() {
-
-  background(220);
-
-// showing tree array
-  for ( var i = 0; i < tree.length; i++) {
-    //tree[1].show(); //replacing root.show, now array
-    //tree[2].show();
-    tree[i].show();
-    tree[i].jitter();
-  }
-
-  //root.show();
-
-  for ( var i = 0; i < leaves.length; i++) {
-    fill(245, 156, 67);
-    ellipse (leaves[i].x, leaves[i].y, 8,8);
-    //leaves[i].show();
-  }
-
 }
-
-
-
-
-
-      //with every mouse press, branch one next
-      //with mouse press in first half of x, branch left
-      //with mouse press in second half of x, branch right
-      //with double click two branches from one
